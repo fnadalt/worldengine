8182a8af91768f11f425f3df6a7e14a6c4cd9341
fnadalt <fnt2006@gmail.com>
2019-07-28 15:21:51 -0300

    error in path construction

--- worldengine/__main__.py
+++ worldengine/__main__.py
@@ -1,6 +1,7 @@
 if __name__ == "__main__":
     from os import path, pardir
     import sys
-    sys.path.append(path.join(path.dirname(path.realpath(__file__)), pardir))
+    sys.path.append(path.join(path.dirname(path.realpath(__file__)), '.'))
+    print(str(sys.path))
     from cli import main
     main.main()
